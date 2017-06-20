c7d2460ddb33d25f17f43d386bfd614e1819de8c
Andrew McConachie <andrew@depht.com>
2017-06-20 11:30:51 -0400

    Hackin on TopThick.py

--- topThick.py
+++ topThick.py
@@ -2,41 +2,68 @@
 
 import os
 import sys
+import random
 
 numTestDomains = 10
 numTopTLDs = 10
 ignoreDomains = ['com', 'net', 'jobs', 'cat', 'mil', 'edu', 'gov', 'int', 'arpa']
 
+def dbg(s):
+#  print s
+  pass
+
 zFiles = os.listdir('zonefiles/')
 
+#dbgFiles = 10 # How many files to read while developing this, remove when finished coding
 tlds = []
 for zf in zFiles:
-  print zf
+#  if len(tlds) >= dbgFiles: # For developing, remove when finished coding
+#    break
+
+  dbg(zf)
   tld = {}
   if zf.find(".txt") == -1:
-    print "This should not happen"
+    dbg("This should not happen")
     continue
 
   zfh = open('zonefiles/' + zf, 'r')
   lines = zfh.read().splitlines()
   zfh.close()
 
+  dbg("after file read")
+
   tld['name'] = lines[0].split(".")[0].strip()
   if tld['name'] in ignoreDomains:
-    print "Ignoring:" + tld['name']
+    dbg("Ignoring:" + tld['name'])
     continue
+
+  dbg("after name split")
 
   tld['rrs'] = []
   for line in lines:
     rr = line.split("\t")
     tld['rrs'].append(rr)
-  
-  tld['count'] = 0
-  for rr in tld['rrs']:
-    if rr[3].lower() == 'a' or rr[3].lower() == 'aaaa':
-      tld['count'] += 1
 
-  if tld['count'] == 0:
+  dbg("after rr split")
+
+  tld['ns'] = []
+  for rr in tld['rrs']:
+    if rr[3].lower() == 'ns':
+      tld['ns'].append(rr[0].split(".")[0])
+
+  dbg("after counting NS records")
+
+  if len(tld['ns']) < numTestDomains:
     continue
-      
-  print tld['name'] + ": " + str(tld['count'])
+
+  tld['domains'] = random.sample(tld['ns'], numTestDomains)
+  for d in tld['domains']:
+    dbg(d + "." + tld['name'])
+
+  dbg(tld['name'] + ": " + str(len(tld['ns'])))
+  tlds.append(tld)
+
+tlds.sort(key=lambda tld: len(tld['ns']), reverse=True)
+
+for tld in tlds:
+  print  tld['name'] + " " + str(len(tld['ns']))
