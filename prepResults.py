f574499f3a15d01fa048fc5b619ddf076484e42f
Andrew McConachie <andrew@depht.com>
2017-08-11 02:29:36 +0000

    Added switch -q for q/h calculations

--- prepResults.py
+++ prepResults.py
@@ -20,13 +20,17 @@
 import os
 import sys
 import argparse
+import datetime
+import re
 
 
 ap = argparse.ArgumentParser(description='Process results files into CSV')
 ap.add_argument(nargs='+', metavar='file', dest='infile', type=argparse.FileType('r'),
                   default=sys.stdin, help='Results input file if not using stdin')
+ap.add_argument('-q', '--qh', action='store_true', dest='qh', help='Append queries/hour')
 args = ap.parse_args()
 
+ts = {}
 servers = {}
 servers['header'] = ['server', '0-pass', '0-fail', '0-noma',
                        '1-pass', '1-fail', '1-noma',
@@ -45,10 +49,19 @@ for f in args.infile:
         toks =line.split(' ')
         if toks[2] not in servers:
           servers[toks[2]] = []
+          ts[toks[2]] = []
+          ts[toks[2]].append(None)
           for ii in range(len(servers['header']) - 1):
             servers[toks[2]].append(0)
+            if not ii % 3:
+              ts[toks[2]].append(None)
 
         case = int(toks[4].split('case-')[1].split('.')[0])
+        if not ts[toks[2]][case]:
+          ts[toks[2]][case] = toks[0]
+        if len(ts[toks[2]]) == case + 2:
+          ts[toks[2]][case+1] = toks[0]
+
         if toks[1] == 'PASS':
           servers[toks[2]][(case * 3) + 0] += 1
         elif toks[1] == 'NOMA':
@@ -56,15 +69,40 @@ for f in args.infile:
         else:
           servers[toks[2]][(case * 3) + 1] += 1
 
+#print(repr(ts))
 rv = ''
 for h in servers['header']:
   rv += h + ','
+if args.qh:
+  for ii in range((len(servers['header']) - 1) / 3):
+    rv += str(ii) + '-q/h,'
 print(rv.strip(','))
 
+p = re.compile('\d+') # Regex to split timestamps
 for s in servers.iteritems():
   if s[0] == 'header':
     continue
   rv = s[0] + ','
   for c in s[1]:
     rv += str(c) + ','
+  if args.qh:
+    for ii in range(len(ts[s[0]]) - 1): # Handle query/hour calculations
+      queries = sum(s[1][ii * 3: (ii * 3) + 3])
+      if ts[s[0]][ii].find('/') == -1: # Old style dates, %H:%M:%S.%f
+        fHour, fMinute, fSecond, fMs = map(int, p.findall(ts[s[0]][ii]))
+        lHour, lMinute, lSecond, lMs = map(int, p.findall(ts[s[0]][ii+1]))
+        first = datetime.datetime(2017, 01, 15, fHour, fMinute, fSecond, fMs)
+        last = datetime.datetime(2017, 01, 15, lHour, lMinute, lSecond, lMs)
+        if last > first:
+          qh = int(queries / int((last - first).total_seconds() / 3600) )
+        else:
+          qh = int(queries / int((86400 - (first - last).total_seconds()) / 3600))
+      else: # New style dates, %m/%d/%H:%M:%S.%f
+        fMonth, fDay, fHour, fMinute, fSecond, fMs = map(int, p.findall(ts[s[0]][ii]))
+        lMonth, lDay, lHour, lMinute, lSecond, lMs = map(int, p.findall(ts[s[0]][ii+1]))
+
+        first = datetime.datetime(2017, fMonth, fDay, fHour, fMinute, fSecond, fMs)
+        last = datetime.datetime(2017, lMonth, lDay, lHour, lMinute, lSecond, lMs)
+        qh = int(queries / (last - first).total_seconds() * 3600)
+      rv += str(qh) + ','
   print(rv.strip(','))
